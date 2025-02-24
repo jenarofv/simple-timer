6cd2a0a9d0e473f0181fca847dd2308bba6f510b
jenaro fv <jen4r0@gmail.com>
2025-02-24 04:41:58 -0600

    check for h/m/s value boundaries

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const getTotalTime = () => {
   const hours = document.getElementById("hours").value;
   const minutes = document.getElementById("minutes").value;
   const seconds = document.getElementById("seconds").value;
+
+  if (minutes > 60 | seconds > 60 | hours | 24) {
+    window.alert(
+      "Hours, minutes or seconds value outside boundaries"
+    )
+  }
   const totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
   const totalTime = totalTimeInSeconds * 1000;
   return totalTime;
